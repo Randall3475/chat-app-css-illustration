6d0ef2e6f52a7f9260ee0a9f980c0eba5650595e
Randall3475 <132885679+Randall3475@users.noreply.github.com>
2023-05-23 17:32:06 +0200

    Update globalStyle.jsx

--- src/globalStyle.jsx
+++ src/globalStyle.jsx
@@ -7,16 +7,12 @@ const GlobalStyle = createGlobalStyle`
   }
   *, *::before, *::after {
   box-sizing: border-box;
-  }
-  html {
-    color-scheme: dark light;
-  }
+  }  
   html, body {
     font-family: 'Rubik', sans-serif;
     font-size: 10px;
     min-height: 100vh;
     overflow: hidden;
-    background-color: #F2F2F2;
     @media only screen and (max-width: 1200px) {
       overflow: auto;
   }

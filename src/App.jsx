09d296eff1cde2c46dc34af814b08e7f62f5ce0b
Randall3475 <132885679+Randall3475@users.noreply.github.com>
2023-06-14 08:55:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ const Main = styled.main`
     width: 510px;
     height: 1018px;
     left: 1115px;
-    top: 318px;
+    top: 200px;
     background: linear-gradient(225deg, #E844FF 0%, #8739FF 100%);
     mix-blend-mode: normal;
     opacity: 0.03;
